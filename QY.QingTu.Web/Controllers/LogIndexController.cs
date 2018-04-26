570f799d2e489ca91e3ff3d02239bf543b3ebfc8
Hizcy <1256929630@qq.com>
2018-04-26 22:47:43 +0800

    测试vs提交

---
+++ QY.QingTu.Web/Controllers/LogIndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QY.QingTu.Web.Controllers
+{
+    public class LogIndexController : Controller
+    {
+        // GET: LogIndex
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ QY.QingTu.Web/Scripts/bc_orderall.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+$(document).ready(function(){
+	$(".order_detail_flist tr:even").css({"background":"#f9f9fa","border-top":"1px solid #e1e6eb"});
+});
